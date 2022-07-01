28de7d528f217ff9d5048be3086bb5416f580940
Anumol K Mani <anumol.km32@gmail.com>
2022-07-01 23:38:46 +0400

    2nd commit

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -5,6 +5,13 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const{coktails,loading}=useGlobalContext();
+  if(loading){
+    return<Loading/>
+  }
+  if(coktails.length<1){
+    return(<h2 className='section-title'>no coktails matched your search</h2>)
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
